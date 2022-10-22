65e28db551354d15258db2f311fa1c6a7d3c298c
Martin Robertz <dream-master@gmx.net>
2022-10-22 12:26:07 +0200

    update

--- src/main/java/singulariteam/eternalsingularity/EternalSingularityMod.java
+++ src/main/java/singulariteam/eternalsingularity/EternalSingularityMod.java
@@ -1,7 +1,5 @@
 package singulariteam.eternalsingularity;
 
-import static singulariteam.eternalsingularity.Reference.*;
-
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -14,6 +12,8 @@ import net.minecraft.item.Item;
 import org.apache.logging.log4j.Logger;
 import singulariteam.eternalsingularity.item.EternalSingularityItem;
 import singulariteam.eternalsingularity.proxy.CommonProxy;
+
+import static singulariteam.eternalsingularity.Reference.*;
 
 @Mod(
         modid = MOD_ID,
--- src/main/java/singulariteam/eternalsingularity/Reference.java
+++ src/main/java/singulariteam/eternalsingularity/Reference.java
@@ -3,7 +3,7 @@ package singulariteam.eternalsingularity;
 public final class Reference {
     public static final String MOD_ID = "eternalsingularity";
     public static final String MOD_NAME = "Eternal Singularity";
-    public static final String MOD_VERSION = "1.7.10-.1.0.4b";
+    public static final String MOD_VERSION = "GRADLETOKEN_VERSION";
     public static final String MC_VERSION = "[1.7.10]";
     static final String COMMON_PROXY = "singulariteam.eternalsingularity.proxy.CommonProxy";
     static final String CLIENT_PROXY = "singulariteam.eternalsingularity.proxy.ClientProxy";
